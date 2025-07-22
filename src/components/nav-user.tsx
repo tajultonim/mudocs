86a625fecac6fc0e1d088d995f571398f339dd85
Tajul Tonim <tajul.islam.tonim@gmail.com>
2025-07-23 05:54:24 +0600

    Display username as fallback for user name in NavUser component

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -95,7 +95,7 @@ export function NavUser() {
                   </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate font-medium">{user.name||user.username}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
               </div>
